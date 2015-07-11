3afc002ecef9f1015b38f7f6977d1cdf1f76a902
Jérémy Morin <jer.morin@free.fr>
2015-07-11 10:15:24 +0200

    feat(design) hover effect

--- client/books/controllers/books.ng.js
+++ client/books/controllers/books.ng.js
@@ -1,5 +1,11 @@
 'use strict';
 
-angular.module('mLibrary').controller('BooksCtrl', ['$scope', '$meteor', function ($scope, $meteor) {
+angular.module('mLibrary').controller('BooksCtrl', ['$scope', '$meteor', '$location',
+    function ($scope, $meteor, $location) {
         $scope.books = $meteor.collection(Books);
+
+
+        $scope.goDetails = function(book){
+            $location.path('/books/' + book._id);
+        }
     }]);
