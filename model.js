910b1fd573ceb6072f96088d53d6a1c1101eb4e5
Jérémy Morin <jer.morin@free.fr>
2015-08-04 21:17:15 +0200

    feat(auth) mongo secure to limit request

--- model.js
+++ model.js
@@ -1,1 +1,15 @@
 Books = new Mongo.Collection('books');
+
+Books.allow({
+    insert: function (userId, book) {
+        return userId && book.owner === userId;
+    },
+    update: function (userId) {
+        if (!userId) return false;
+        return true;
+    },
+    remove: function (userId, book) {
+        if (userId !== book.owner) return false;
+        return true;
+    }
+}); 
